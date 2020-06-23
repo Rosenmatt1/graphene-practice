a89b9e719d00d04e207cf1ab489281ff3ce1e9e4
Mateo <rosenmatt1@gmail.com>
2020-06-23 16:59:08 -0600

    Added a Create User and Posts classes

--- schema.py
+++ schema.py
@@ -1,26 +1,118 @@
 import graphene
 import json
+import uuid
+from datetime import datetime
 
-class Query(graphene.ObjectType): 
+
+class Post(graphene.ObjectType):
+    title = graphene.String()
+    content = graphene.String()
+
+
+class User(graphene.ObjectType):
+    id = graphene.ID(default_value=str(uuid.uuid4()))
+    username = graphene.String()
+    created_at = graphene.DateTime(default_value=datetime.now())
+
+
+class Query(graphene.ObjectType):
+    users = graphene.List(User, limit=graphene.Int())
     hello = graphene.String()
     is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return "world"
-    
+
     def resolve_is_admin(self, info):
         return True
 
-schema = graphene.Schema(query=Query, auto_camelcase=False)
-# the autocamelcase false allows the bellow is_admin to be written in snake case
+    def resolve_users(self, info, limit=None):
+        # None is a fallback value in case a limit is is passed.  hence, making limit optional.
+        return [
+            User(id="1", username="Fred", created_at=datetime.now()),
+            User(id="2", username="George", created_at=datetime.now()),
+            User(id="3", username="Maddie", created_at=datetime.now())
+        ][:limit]
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
+
+    class Arguments:
+        username = graphene.String()
+
+    def mutate(self, info, username):
+        user = User(username=username)
+        return CreateUser(user=user)
+
+
+class CreatePost(graphene.Mutation):
+    post = graphene.Field(Post)
+
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    def mutate(self, info, title, content):
+        if info.context.get('is_anonymous'):
+            raise Exception('Not authenticated')
+        post = Post(title=title, content=content)
+        return CreatePost(post=post)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+    create_post = CreatePost.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+#  auto_camelcase=False, pass as 2nd argument in query as the autocamelcase false allows the bellow is_admin to be written in snake case
+
+# ! means it is required
 
 result = schema.execute(
     '''
-    {
-        is_admin
+    mutation {
+        createPost(title: "Hello", content: "World") {
+            post {
+                title
+                content
+            }
+        }
     }
-    '''
+    ''',
+    context={ 'is_anonymous': True}
 )
+
+#   query {
+#         users(limit: 1) {
+#             id
+#             username
+#             createdAt
+#         }
+#     }
+
+#  query getUsersQuery ($limit: Int) {
+#         users(limit: $limit) {
+#             id
+#             username
+#             createdAt
+#         }
+#     }
+#     ''',
+#     variable_values={'limit': '1'}
+
+#   {
+#     mutation ($username: String) {
+#         createUser(username: $username) {
+#             user {
+#                 id
+#                 username
+#                 createdAt
+#             }
+#         }
+#     }
+# }
 
 dictResult = dict(result.data.items())
 print(json.dumps(dictResult, indent=2))
